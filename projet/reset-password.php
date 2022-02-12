c7411dd90d7bcc4e9e30c59a53219c0af757a08a
salma-essabar123 <96539285+salma-essabar123@users.noreply.github.com>
2022-02-12 16:15:26 +0100

    Update reset-password.php

--- projet/reset-password.php
+++ projet/reset-password.php
@@ -85,20 +85,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <h2>Reset Password</h2>
         <p>Please fill out this form to reset your password.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
-            <div class="form-group">
+           
                 <label>New Password</label>
                 <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
                 <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
+          
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
+            
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
-            </div>
+           
         </form>
     </div>    
 </body>
