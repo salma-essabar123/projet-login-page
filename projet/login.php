5265bd2430575c6fca0da81f4866fbc31ee432df
salma-essabar123 <96539285+salma-essabar123@users.noreply.github.com>
2022-02-12 16:11:56 +0100

    Update login.php

--- projet/login.php
+++ projet/login.php
@@ -106,24 +106,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         <?php 
         if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            echo  $login_err ;
         }        
         ?>
 
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
+            
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
+               
+            
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
+            
+          
                 <input type="submit" class="btn btn-primary" value="Login">
-            </div>
+            
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>
